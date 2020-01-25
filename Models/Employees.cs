7db27c9678723f396d7743f8c454c269ba2b3023
Jabro <abuissan@gmail.com>
2020-01-25 15:26:42 +0700

    pindahan

---
+++ Models/Employees.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using CSHARPMVC.Models;
+using System.Web.Mvc;
+
+namespace CSHARPMVC.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string employee_name { get; set; }
+        public string employee_salary { get; set; }
+        public int employee_age { get; set; }
+
+    }
+
+}
